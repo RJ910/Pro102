ab550403bf5808fd936f8817e1c0ecae491f0ed0
RJ910 <75748617+RJ910@users.noreply.github.com>
2023-03-14 23:59:52 +0530

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+from_dir="C:/Users/Raj/Downloads"
+to_dir="C:/Whitehatjr/Downloadedimages"
+
+list_of_files= os.listdir(from_dir)
+#print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension= os.path.splitext(file_name)
+   # print(name)
+    #print(extension)
+
+    if extension == "" :
+     continue
+
+    if extension in [".png", ".jpg", ".jpeg", ".gif", ".jfif"]:
+
+     path1= from_dir + "/" + file_name
+     path2= to_dir + "/" + "Image_files"
+     path3= to_dir + "/" + "Image_files" + "/" + file_name
+
+     if os.path.exists(path2):
+       print("Moving " + file_name + "...")
+
+       shutil.move(path1, path3)
+
+     else:
+       os.makedirs(path2)
+       print("Moving" + file_name + "...")
+       shutil.move(path1, path3)
+print(os.listdir(to_dir))
